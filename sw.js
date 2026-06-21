fe8a240c5a79767896be0481ae8e5552816a6b72
coeeennn-maker <coeeennn@gmail.com>
2026-06-21 17:58:53 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'btc-dca-v1';
+const SHELL = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+
+  if (url.includes('blockchain.info')) {
+    e.respondWith(
+      fetch(e.request)
+        .then(r => {
+          const clone = r.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return r;
+        })
+        .catch(() => caches.match(e.request).then(r => r || Response.error()))
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
